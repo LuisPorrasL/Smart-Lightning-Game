0c58066da623ae66304805275423eb584c35d759
Luis Porras <luis.porrasledezma@ucr.ac.cr>
2019-11-14 17:04:04 -0600

    Add almost complete smart poles

---
+++ Smart-Lightning/Assets/Scripts/ColliderController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderController : MonoBehaviour
+{
+
+    public Light smartLight;
+
+    void Start()
+    {
+        smartLight.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        smartLight.enabled = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        smartLight.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Invoke("TurnLightOff", 10);
+    }
+
+    void TurnLightOff()
+    {
+        smartLight.enabled = false;
+    }
+
+}
